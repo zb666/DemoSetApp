ea6e508bcb57e7f28f680f0aaaa2737481326012
zb666 <1360799362@qq.com>
2018-12-22 23:54:05 +0800

    签名板

--- daggerdemo/src/main/java/com/example/daggerdemo/view/SignView.java
+++ daggerdemo/src/main/java/com/example/daggerdemo/view/SignView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PathMeasure;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -18,6 +19,8 @@ public class SignView extends View {
     private android.graphics.Path mPath;
     private int mStartX;
     private int mStartY;
+    private android.graphics.Path mMeasurePath;
+    private android.graphics.Path dst;
 
     public SignView(Context context) {
         this(context, null);
@@ -38,6 +41,16 @@ public class SignView extends View {
         mPaintBezier.setStyle(Paint.Style.STROKE);
         mPaintBezier.setColor(Color.GREEN);
         mPath = new android.graphics.Path();
+
+        mMeasurePath = new android.graphics.Path();
+        mMeasurePath.addRect(-50,-50,50,50, android.graphics.Path.Direction.CW);
+        //构造出PathMeasure路径
+        PathMeasure pathMeasure = new PathMeasure(mMeasurePath,false);
+        //截取路径
+        dst =  new android.graphics.Path();
+        //截取该路径0-150 然后放置到dst路径之下
+        pathMeasure.getSegment(0,150,dst,true);
+
     }
 
     @Override
@@ -48,14 +61,13 @@ public class SignView extends View {
                 mPath.reset();
                 mStartX = (int) event.getX();
                 mStartY = (int) event.getY();
-                mPath.moveTo(mStartX,mStartY);
+                mPath.moveTo(mStartX, mStartY);
                 break;
             case MotionEvent.ACTION_MOVE:
                 int mNextBezierX = (int) event.getX();
                 int mNextBezierY = (int) event.getY();
                 int mQuadX = (mNextBezierX + mStartX) / 2;
                 int mQuadY = (mNextBezierY + mStartY) / 2;
-
                 mPath.quadTo(mStartX, mStartY, mQuadX, mQuadY);
                 postInvalidate();
                 mStartX = mNextBezierX;
@@ -69,6 +81,19 @@ public class SignView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawPath(mPath, mPaintBezier);
+        canvas.drawPath(dst,mPaintBezier);
     }
 
+    /**
+     * 当前屏幕的大小
+     * @param w 宽度
+     * @param h 高度
+     * @param oldw 旧的宽度
+     * @param oldh 旧的高度
+     */
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.d("BobV","onSizeChanged");
+    }
 }
